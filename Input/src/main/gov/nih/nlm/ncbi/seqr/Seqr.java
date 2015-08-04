83e42f855e358e6b4019a9ff7a90560f2e12099c
Justin Payne <justin.payne@fda.hhs.gov>
2015-08-04 16:21:39 -0400

    CLI now accepts and breaks out -outfmt field strings index now accepts 1+ fasta files

--- Input/src/main/gov/nih/nlm/ncbi/seqr/Seqr.java
+++ Input/src/main/gov/nih/nlm/ncbi/seqr/Seqr.java
@@ -2,14 +2,15 @@ package gov.nih.nlm.ncbi.seqr;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 import net.sourceforge.argparse4j.ArgumentParsers;
-import net.sourceforge.argparse4j.annotation.Arg;
 import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.ArgumentGroup;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
 import net.sourceforge.argparse4j.inf.Subparsers;
 
 import org.apache.solr.client.solrj.SolrServer;
@@ -32,21 +33,34 @@ public class Seqr {
                 .description("SEQR, now for shells.")
                 .version("${prog} 0.0.1a");
 
+        ArgumentGroup unused = parser.addArgumentGroup("unused").description("Unused compatibility arguments from BLASTP");
         //parser.addArgument("command").type(String.class).dest("command").help("Non-optional command").choices("search", "index", "load").required(true);
         Subparsers subprsrs = parser.addSubparsers().description("SEQR commands").metavar("COMMAND");
-        subprsrs.addParser("search").help("look for something");
-        subprsrs.addParser("index").help("create an index");
-        subprsrs.addParser("load").help("load something");
+        Subparser search = subprsrs.addParser("search").help("look for something");
+        Subparser index = subprsrs.addParser("index").help("create an index");
+        
+        //add options for search
+        search.addArgument("--solr_query").type(String.class).help("filtering query in Solr query language");
+        search.addArgument("--index_file").type(Arguments.fileType().acceptSystemIn().verifyCanRead()).help("pre-calculated index file");
+        search.addArgument("--num_alignments").type(Integer.class).help("number of results to return");
+        search.addArgument("--start_alignments").type(Integer.class).help("begin output at the Nth result").metavar("N");
+        
+        //add special outformat parser
+        search.addArgument("--outfmt").type(String.class).dest("format").help("options for formatted output").nargs("+");
+        
+        //add required, options for index
+        index.addArgument("input_files").type(String.class).nargs("+").required(true);
         
         parser.addArgument("-d", "--db").type(String.class).dest("solr_url_or_path").help("URL of Solr server, or path to Solr directory").required(true);
         
         //add version
         parser.addArgument("--version").action(Arguments.version());
+        parser.addArgument("--citation").action(Arguments.storeTrue());
+        
+        
         
         //add options
-        parser.addArgument("--outfmt").type(String.class).dest("format").help("options for formatted output");
-        
-        parser.addArgument("-o", "--out").type(String.class).dest("output_file").help("path to output directory");
+        parser.addArgument("-o", "--out").type(Arguments.fileType()).dest("output_file").help("path to output directory");
         parser.addArgument("--query").type(String.class).dest("input_file").help("query file for input");
         //add flags
         parser.addArgument("--parse_deflines").help("derive metadata from FASTA deflines and comments").action(Arguments.storeTrue());;
@@ -57,7 +71,7 @@ public class Seqr {
         
         
         //add unused flags
-        ArgumentGroup unused = parser.addArgumentGroup("unused").description("Unused compatibility arguments from BLASTP");
+       
         unused.addArgument("--html").help("unused").action(Arguments.storeTrue());
         unused.addArgument("--lcase_masking").help("unused").action(Arguments.storeTrue());;
         unused.addArgument("--remote").help("unused").action(Arguments.storeTrue());;
@@ -66,49 +80,49 @@ public class Seqr {
         unused.addArgument("--use_sw_tback").help("unused").action(Arguments.storeTrue());;
 
         //add unused options
-        unused.addArgument("--import_search_strategy").type(String.class).dest("filename").help("unused");
-        unused.addArgument("--export_search_strategy").type(String.class).dest("filename").help("unused");
-        unused.addArgument("--task").type(String.class).dest("task_name").help("unused");    
-        unused.addArgument("--dbsize").type(String.class).dest("num_letters").help("unused");
-        unused.addArgument("--gilist").type(String.class).dest("filename").help("unused");
-        unused.addArgument("--seqidlist").type(String.class).dest("filename").help("unused");
-        unused.addArgument("--negative_gilist").type(String.class).dest("filename").help("unused");
-        unused.addArgument("--entrez_query").type(String.class).dest("entrez_query").help("unused");
-        unused.addArgument("--db_soft_mask").type(String.class).dest("filtering_algorithm").help("unused");
-        unused.addArgument("--db_hard_mask").type(String.class).dest("filtering_algorithm").help("unused");
-        unused.addArgument("--subject").type(String.class).dest("subject_input_file").help("unused");
-        unused.addArgument("--subject_loc").type(String.class).dest("range").help("unused"); 
-        unused.addArgument("--evalue").type(String.class).dest("evalue").help("unused");
-        unused.addArgument("--word_size").type(Integer.class).help("unused");
-        unused.addArgument("--gapopen").type(String.class).dest("open_penalty").help("unused");
-        unused.addArgument("--gapextend").type(String.class).dest("extend_penalty").help("unused");
-        unused.addArgument("--qcov_hsp_perc").type(Float.class).help("unused");
-        unused.addArgument("--max_hsps").type(Integer.class).help("unused");
-        unused.addArgument("--xdrop_ungap").type(Float.class).help("unused");
-        unused.addArgument("--xdrop_gap").type(Float.class).help("unused");
-        unused.addArgument("--xdrop_gap_final").type(Float.class).help("unused");
-        unused.addArgument("--searchsp").type(Integer.class).help("unused");
-        unused.addArgument("--sum_stats").type(String.class).dest("bool").help("unused");
-        unused.addArgument("--seg").type(String.class).dest("SEG_options").help("unused");
-        unused.addArgument("--soft_masking").type(String.class).dest("soft_masking").help("unused");
-        unused.addArgument("--matrix").type(String.class).dest("matrix_name").help("unused");
-        unused.addArgument("--threshold").type(Float.class).help("unused");
-        unused.addArgument("--culling_limit").type(Integer.class).help("unused");
-        unused.addArgument("--best_hit_overhang").type(Float.class).help("unused");
-        unused.addArgument("--best_hit_score_edge").type(Float.class).help("unused");
-        unused.addArgument("--window_size").type(Integer.class).help("unused");
-        unused.addArgument("--query_loc").type(String.class).dest("range").help("unused");
-        unused.addArgument("--num_descriptions").type(Integer.class).help("unused");
-        unused.addArgument("--num_alignments").type(Integer.class).help("unused");
-        unused.addArgument("--line_length").type(String.class).dest("line_length").help("unused");
-        unused.addArgument("--max_target_seqs").type(String.class).dest("num_sequences").help("unused");
-        unused.addArgument("--num_threads").type(Integer.class).help("unused");
-        unused.addArgument("--comp_based_stats").type(String.class).dest("compo").help("unused");
+        unused.addArgument("--import_search_strategy").type(String.class).dest("filename").help("unused").nargs(1);
+        unused.addArgument("--export_search_strategy").type(String.class).dest("filename").help("unused").nargs(1);
+        unused.addArgument("--task").type(String.class).dest("task_name").help("unused").nargs(1);    
+        unused.addArgument("--dbsize").type(Integer.class).dest("num_letters").help("unused").nargs(1);
+        unused.addArgument("--gilist").type(String.class).dest("filename").help("unused").nargs(1);
+        unused.addArgument("--seqidlist").type(String.class).dest("filename").help("unused").nargs(1);
+        unused.addArgument("--negative_gilist").type(String.class).dest("filename").help("unused").nargs(1);
+        unused.addArgument("--entrez_query").type(String.class).dest("entrez_query").help("unused").nargs(1);
+        unused.addArgument("--db_soft_mask").type(String.class).dest("filtering_algorithm").help("unused").nargs(1);
+        unused.addArgument("--db_hard_mask").type(String.class).dest("filtering_algorithm").help("unused").nargs(1);
+        unused.addArgument("--subject").type(String.class).dest("subject_input_file").help("unused").nargs(1);
+        unused.addArgument("--subject_loc").type(String.class).dest("range").help("unused").nargs(1); 
+        unused.addArgument("--evalue").type(String.class).dest("evalue").help("unused").nargs(1);
+        unused.addArgument("--word_size").type(Integer.class).help("unused").nargs(1);
+        unused.addArgument("--gapopen").type(String.class).dest("open_penalty").help("unused").nargs(1);
+        unused.addArgument("--gapextend").type(String.class).dest("extend_penalty").help("unused").nargs(1);
+        unused.addArgument("--qcov_hsp_perc").type(Float.class).help("unused").nargs(1);
+        unused.addArgument("--max_hsps").type(Integer.class).help("unused").nargs(1);
+        unused.addArgument("--xdrop_ungap").type(Float.class).help("unused").nargs(1);
+        unused.addArgument("--xdrop_gap").type(Float.class).help("unused").nargs(1);
+        unused.addArgument("--xdrop_gap_final").type(Float.class).help("unused").nargs(1);
+        unused.addArgument("--searchsp").type(Integer.class).help("unused").nargs(1);
+        unused.addArgument("--sum_stats").type(String.class).dest("bool").help("unused").nargs(1);
+        unused.addArgument("--seg").type(String.class).dest("SEG_options").help("unused").nargs(1);
+        unused.addArgument("--soft_masking").type(String.class).dest("soft_masking").help("unused").nargs(1);
+        unused.addArgument("--matrix").type(String.class).dest("matrix_name").help("unused").nargs(1);
+        unused.addArgument("--threshold").type(Float.class).help("unused").nargs(1);
+        unused.addArgument("--culling_limit").type(Integer.class).help("unused").nargs(1);
+        unused.addArgument("--best_hit_overhang").type(Float.class).help("unused").nargs(1);
+        unused.addArgument("--best_hit_score_edge").type(Float.class).help("unused").nargs(1);
+        unused.addArgument("--window_size").type(Integer.class).help("unused").nargs(1);
+        unused.addArgument("--query_loc").type(String.class).dest("range").help("unused").nargs(1);
+        unused.addArgument("--num_descriptions").type(Integer.class).help("unused").nargs(1);
+        unused.addArgument("--line_length").type(String.class).dest("line_length").help("unused").nargs(1);
+        unused.addArgument("--max_target_seqs").type(String.class).dest("num_sequences").help("unused").nargs(1);
+        unused.addArgument("--num_threads").type(Integer.class).help("unused").nargs(1);
+        unused.addArgument("--comp_based_stats").type(String.class).dest("compo").help("unused").nargs(1);
         
         
 
         try {
             Namespace space = parser.parseArgs(args);
+            System.out.println(space);
             handleCommand(space);
         } catch (ArgumentParserException e) {
         	System.out.println(e);
@@ -141,7 +155,13 @@ public class Seqr {
     		//solr server is local
     		CoreContainer container = new CoreContainer(solrString);
             container.load();
-           	solrServer = new EmbeddedSolrServer(container, "default_collection");
+           	solrServer = new EmbeddedSolrServer(container, "Collection1");
+    	}
+    	
+    	//handle outfmt if present
+    	if (space.get("format") != null){
+    		List<String> outputFields = space.getList("format");
+    		Integer outputCode = Integer.parseInt(outputFields.remove(0));
     	}
     	
     	//pass on to appropriate subcommand
