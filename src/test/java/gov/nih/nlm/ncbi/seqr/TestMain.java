132e3a526460762cc77f23ea81f87b89307900b9
michaelpanciera <michael.panciera.work@gmail.com>
2015-08-06 13:04:16 -0400

    Merge branch 'master' of /home/michaelpanciera/clones/seqr_hackathon_v002 with conflicts.

--- src/test/java/gov/nih/nlm/ncbi/seqr/TestMain.java
+++ src/test/java/gov/nih/nlm/ncbi/seqr/TestMain.java
@@ -1,44 +1,44 @@
-package gov.nih.nlm.ncbi.seqr.test;
+package gov.nih.nlm.ncbi.seqr;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.FileFilterUtils;
-import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
-import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.params.CommonParams;
-import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.core.CoreContainer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.util.Collection;
 
 //import org.codehaus.jackson.JsonNode;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
 
-public class Test {
+import java.io.IOException;
 
+public class TestMain {
+    private gov.nih.nlm.ncbi.seqr.SeqrController control;
+    EmbeddedSolrServer solrServer;
 
-
-    public static void main(String[] args) throws Exception {
+    @Before
+    public void setUp() throws SolrServerException, InterruptedException, IOException {
         String solrString = "testdata/solr";
         CoreContainer container = new CoreContainer(solrString);
         container.load();
-        EmbeddedSolrServer solrServer = new EmbeddedSolrServer(container, "sequence");
-        gov.nih.nlm.ncbi.seqr.SeqrController control = new gov.nih.nlm.ncbi.seqr.SeqrController(solrServer);
-
-
+        solrServer = new EmbeddedSolrServer(container, "sequence");
+        control = new gov.nih.nlm.ncbi.seqr.SeqrController(solrServer);
         control.loadJSONDir("testdata/data");
+    }
+
+    @Test
+    public void testSolrSimple() throws Exception {
+
+
         String seq = "AGSYLLEELFEGHLEKECWEEICVYEEAREVFEDDETTDEFWRTYMGGSPCASQPCLNNGSCQDSIRGYACTCAPGYEGPNCAFAESECHPLRLDGCQHFCYPGPESYTCSCARGHKLGQDRRSCLPHDRCACGTLGPECCQRPQGSQQNLLPFPWQVKLTNSEGKDFCGGVLIQDNFVLTTATCSLLYANISVKTRSHFRLHVRGVHVHTRFEADTGHNDVALLDLARPVRCPDAGRPVCTADADFADSVLLPQPGVLGGWTLRGREMVPLRLRVTHVEPAECGRALNATVTTRTSCERGAAAGAARWVAGGAVVREHRGAWFLTGLLGAAPPEGPGPLLLIKVPRYALWLRQVTQQPSRASPRGDRGQGRDGEPVPGDRGGRWAPTALPPGPLV";
         SolrDocumentList results = control.search(seq);
         for (SolrDocument document : results) {
             System.out.println(document);
         }
-        assert results.size() == 1;
+        Assert.assertEquals(1, results.size());
         solrServer.shutdown();
     }
 }
