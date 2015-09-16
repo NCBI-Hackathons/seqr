683ff503e44290d14885f0e921ca2e402b29cb32
Bo Yu <bo.yu2@nih.gov>
2015-09-16 12:01:35 -0400

    add fasta file parser, refactor StreamParser

--- src/test/java/gov/nih/nlm/ncbi/seqr/TestMain.java
+++ src/test/java/gov/nih/nlm/ncbi/seqr/TestMain.java
@@ -1,19 +1,20 @@
 package gov.nih.nlm.ncbi.seqr;
 
+import gov.nih.nlm.ncbi.seqr.solr.FastaStreamParser;
+import gov.nih.nlm.ncbi.seqr.solr.LoadLargeFile2SolrServer;
 import gov.nih.nlm.ncbi.seqr.solr.SeqrController;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.core.CoreContainer;
+import java.io.File;
 
 //import org.codehaus.jackson.JsonNode;
 
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.Before;
 
 import java.io.IOException;
 
@@ -28,6 +29,10 @@ CoreContainer container ;
         solrServer = new EmbeddedSolrServer(container, "sequence");
         control = new SeqrController(solrServer);
         control.loadJSONDir("testdata/data");
+
+        LoadLargeFile2SolrServer server = new LoadLargeFile2SolrServer(solrServer);
+        server.loadFastaFile(new File("testdata/data/short.fasta"));
+
     }
 
     public void tearDown() {
@@ -35,11 +40,19 @@ CoreContainer container ;
         container.shutdown();
     }
 
+    @Test
+    public void testFastaSolrImport() throws Exception {
+        LoadLargeFile2SolrServer server = new LoadLargeFile2SolrServer(solrServer);
+        server.loadFastaFile(new File("testdata/data/short.fasta"));
+
+    }
 
     @Test
     public void testAbSolrSimple() throws Exception {
         setUp();
-        String seq = "AGSYLLEELFEGHLEKECWEEICVYEEAREVFEDDETTDEFWRTYMGGSPCASQPCLNNGSCQDSIRGYACTCAPGYEGPNCAFAESECHPLRLDGCQHFCYPGPESYTCSCARGHKLGQDRRSCLPHDRCACGTLGPECCQRPQGSQQNLLPFPWQVKLTNSEGKDFCGGVLIQDNFVLTTATCSLLYANISVKTRSHFRLHVRGVHVHTRFEADTGHNDVALLDLARPVRCPDAGRPVCTADADFADSVLLPQPGVLGGWTLRGREMVPLRLRVTHVEPAECGRALNATVTTRTSCERGAAAGAARWVAGGAVVREHRGAWFLTGLLGAAPPEGPGPLLLIKVPRYALWLRQVTQQPSRASPRGDRGQGRDGEPVPGDRGGRWAPTALPPGPLV";
+        //String seq = "MAFSAEDVLKEYDRRRRMEALLLSLYYPNDRKLLDYKEWSPPRVQVECPKAPVEWNNPPSEKGLIVGHFSGIKYKGEKAQASEVDVNKMCCWVSKFKDAMRRYQGIQTCKIPGKVLSDLDAKIKAYNLTVEGVEGFVRYSRVTKQHVAAFLKELRHSKQYENVNLIHYILTDKRVDIQHLEKDLVKDFKALVESAHRMRQGHMINVKYILYQLLKKHGHGPDGPDILTVKTGSKGVLYDDSFRKIYTDLGWKFTPL";
+        String seq = "MSFKVYDPIAELIATQFPTSNPDLQIINNDVLVVSPHKITLPMGPQNAGDVTNKAYVDQAVMSAAVPVASSTTVGTIQMAGDLEGSSGTNPIIAANKITLNKLQKIGPKMVIGNPNSDWNNTQEIELDSSFRIVDNRLNAGIVPISSTDPNKSNTVIPAPQQNGLFYLDSSGRVWVWAEHYYKCITPSRYISKWMGVGDFQELTVGQSVMWDSGRPSIETVSTQGLEVEWISSTNFTLSSLYLIPIVVKVTICIPLLGQPDQMAKFVLYSVSSAQQPRTGIVLTTDSSRSSAPIVSEYITVNWFEPKSYSVQLKEVNSDSGTTVTICSDKWLANPFLDCWITIEEVG";
+        //String seq = "AGSYLLEELFEGHLEKECWEEICVYEEAREVFEDDETTDEFWRTYMGGSPCASQPCLNNGSCQDSIRGYACTCAPGYEGPNCAFAESECHPLRLDGCQHFCYPGPESYTCSCARGHKLGQDRRSCLPHDRCACGTLGPECCQRPQGSQQNLLPFPWQVKLTNSEGKDFCGGVLIQDNFVLTTATCSLLYANISVKTRSHFRLHVRGVHVHTRFEADTGHNDVALLDLARPVRCPDAGRPVCTADADFADSVLLPQPGVLGGWTLRGREMVPLRLRVTHVEPAECGRALNATVTTRTSCERGAAAGAARWVAGGAVVREHRGAWFLTGLLGAAPPEGPGPLLLIKVPRYALWLRQVTQQPSRASPRGDRGQGRDGEPVPGDRGGRWAPTALPPGPLV";
         SolrDocumentList results = control.search(seq);
         for (SolrDocument document : results) {
             System.out.println(document);
@@ -54,4 +67,12 @@ CoreContainer container ;
         Seqr.main(args);
 
         }
+
+    @Test
+    public void testFastaToJsonFileConversion() throws Exception {
+        FastaStreamParser converter = new FastaStreamParser("testdata/data/short.fasta");
+        converter.convertToJsonFile("testdata/data/short.json");
+    }
+
+
 }
